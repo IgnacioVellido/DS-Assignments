8e014455e0e0e492fd2b45f86ac835289a7d5ca3
Ignacio Vellido <ignaciove@correo.ugr.es>
2020-12-03 13:23:51 +0100

    Update

--- ProgDat/Python/proyecto.py
+++ ProgDat/Python/proyecto.py
@@ -1,22 +1,18 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 ================================================================================
 Cardiotocogram Dataset
 Ignacio Vellido Expósito
 ================================================================================
-3 métodos de predicción + PCA (preprocesado)
-(poner alguna gráfica también)
 
 Data Set Information:
-
-2126 fetal cardiotocograms (CTGs) were automatically processed and the respective 
-diagnostic features measured. The CTGs were also classified by three expert 
-obstetricians and a consensus classification label assigned to each of them. 
-Classification was both with respect to a morphologic pattern (A, B, C. ...) 
-and to a fetal state (N, S, P). Therefore the dataset can be used either for 
-10-class or 3-class experiments.
+    2126 fetal cardiotocograms (CTGs) were automatically processed and the respective 
+    diagnostic features measured. The CTGs were also classified by three expert 
+    obstetricians and a consensus classification label assigned to each of them. 
+    Classification was both with respect to a morphologic pattern (A, B, C. ...) 
+    and to a fetal state (N, S, P). Therefore the dataset can be used either for 
+    10-class or 3-class experiments.
 
 Exam data		
     FileName	of CTG examination		
@@ -73,19 +69,22 @@ medidas usadas en la clasificación.
 ################################################################################
 
 import pandas as pd
-
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Preprocesamiento
+from sklearn.preprocessing import Normalizer
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
 
 # Algoritmos
 from sklearn.svm import SVC
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
 
 # Evaluación
 from sklearn.metrics import classification_report, \
@@ -96,33 +95,78 @@ from sklearn.metrics import classification_report, \
 # Lectura
 ################################################################################
 
-# Cargamos los datos (Raw Data nos es más cómodo que Data)
+# Cargamos los datos (sheet Raw Data nos es más cómodo que Data)
 data = pd.read_excel("data/CTG.xls", "Raw Data")
 
 # Eliminamos las 3 últimas filas que solo contienen valores de máximos y mínimos
 data = data[:-3]
 
-# Eliminamos las primera fila que está vacía
+# Eliminamos la primera fila que está vacía
 data = data[1:]
 
-# Eliminamos la primeras dos columnas pues no contienen información relevante
-# para la clasificación
+# Eliminamos las columnas que no contienen información relevante para la 
+# clasificación
 removed_columns = ["FileName", "Date", "SegFile", "b", "e", "A", "B", "C", "D",
                     "AD", "DE", "LD", "FS", "SUSP", "CLASS"]
 data = data.drop(columns=removed_columns)
 
-print("Data:")
+print("Datos sin normalizar:")
 print(data)
 
+# Separamos las etiquetas
 labels = data.pop("NSP")
+labels = pd.Categorical(labels)
+labels = labels.rename_categories(["N", "S", "P"])
 labels_names = ["Normal", "Suspect", "Pathologic"]
+
+# Normalizamos los datos
+data_norm = Normalizer().fit(data).transform(data)
+data_norm = pd.DataFrame(data_norm)
+
+################################################################################
+# Visualizaciones
+################################################################################
+
+# ------------------------------------------------------------------------------
+# Boxplots (usamos los datos normalizados para que estén en el mismo rango)
+sns.boxplot(x="variable", y="value", data=pd.melt(data_norm))
+plt.show()
+
+# ------------------------------------------------------------------------------
+# Correlación (https://seaborn.pydata.org/examples/many_pairwise_correlations.html)
+sns.set_theme(style="white")
+
+corr = data.corr()
+
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5)
+plt.show()
+
+# ------------------------------------------------------------------------------
+# Scatterplot
+
+# De 2 variables correladas (puesto que es alta, sería recomendable quitar una)
+sns.scatterplot(data=data, x="Min", y="Width")
+plt.show()
 
 ################################################################################
 # Preprocesamiento
 ################################################################################
 
 # Como tenemos bastantes datos, separamos un 20% para test
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
+# Necesitamos que los datos estén normalizados antes de aplicar PCA
+x_train, x_test, y_train, y_test = train_test_split(data_norm, labels, test_size=0.2)
+
 
 # Calculamos PCA sobre train
 n_components = 5    # Bajo porque los tres modelos que tenemos tienden a 
@@ -137,130 +181,191 @@ x_test_pca = pca.transform(x_test)
 # ------------------------------------------------------------------------------
 # Mostramos algunos gráficos
 
+df = pd.DataFrame(x_train_pca)
+df["Labels"] = y_train
+
+sns.pairplot(df, hue="Labels").fig.suptitle("Componentes de PCA con sus verdaderas etiquetas", y=1.01)
+plt.show()
+
+################################################################################
+# Clasificando con SVM
+################################################################################
+print("------------------------------------------------------------------")
+print("Clasificando con SVM")
+print("------------------------------------------------------------------")
+
+# Declaración de hiperparámetros
+param_grid = {
+    "C": [10, 1e3, 1e5],        # Valores de regularización
+    "gamma": [0.01, 0.001],     #
+    "kernel": ["rbf", "poly"]   # Kernel polinomial o ?
+}
+
+# Test de hiperparámetros con cross-validation de 5 folds
+clf = GridSearchCV(SVC(class_weight="balanced"), 
+                    param_grid)
+clf = clf.fit(x_train_pca, y_train)
+
+print("Mejores hiperparámetros del modelo:")
+print(clf.best_params_)
+print("\nMejor score obtenido:")
+print(clf.best_score_)
+
+# Guardamos el mejor estimador
+best_svm = clf.best_estimator_
+
+#############################
+# Evaluación final del modelo
+#############################
+
+y_pred = clf.predict(x_test_pca)
+
+print("\nResultados de la predicción sobre test:")
+print(classification_report(y_test, y_pred, target_names=labels_names))
+print("Matriz de confusión:")
+print(confusion_matrix(y_test, y_pred))
+
+# ------------------------------------------------------------------------------
+# Mostramos gráficos sobre los resultados
+disp = plot_confusion_matrix(best_svm, x_test_pca, y_test,
+                                display_labels=labels_names, cmap=plt.cm.Blues)
+plt.show()
+
+# Clasificación realizada en test
 df = pd.DataFrame(x_test_pca)
-df["Labels"] = pd.factorize(y_train)
+df["Labels"] = y_test
+df["Pred"] = y_pred
 
-sns.pairplot(df, hue="Labels").savefig("pair.png")
+sns.pairplot(df, hue="Labels").fig.suptitle("Componentes de PCA con sus verdaderas etiquetas", y=1.01)
+plt.show()
 
-# ################################################################################
-# # Clasificando con SVM
-# ################################################################################
-# print("------------------------------------------------------------------")
-# print("Clasificando con SVM")
-# print("------------------------------------------------------------------")
-
-# # Declaración de hiperparámetros
-# param_grid = {
-#     "C": [10, 1e3, 1e5],
-#     "gamma": [0.01, 0.001],
-#     "kernel": ["rbf", "poly"]
-# }
-
-# # Test de hiperparámetros con cross-validation de 5 folds
-# clf = GridSearchCV(SVC(class_weight="balanced"), 
-#                     param_grid)
-# clf = clf.fit(x_train_pca, y_train)
-
-# print("Mejores hiperparámetros del modelo:")
-# print(clf.best_params_)
-# print("\nMejor score obtenido:")
-# print(clf.best_score_)
-
-# # Guardamos el mejor estimador
-# best_svm = clf.best_estimator_
-
-# #############################
-# # Evaluación final del modelo
-# #############################
-
-# y_pred = clf.predict(x_test_pca)
-
-# print("\nResultados de la predicción sobre test:")
-# print(classification_report(y_test, y_pred, target_names=labels_names))
-# print("Matriz de confusión:")
-# print(confusion_matrix(y_test, y_pred))
-
-# # ------------------------------------------------------------------------------
-# # Mostramos gráficos sobre los resultados
-# disp = plot_confusion_matrix(best_svm, x_test_pca, y_test,
-#                                 display_labels=labels_names, cmap=plt.cm.Blues)
-# # plt.show()
-# plt.savefig("prueba.png")
-
-# ################################################################################
-# # Clasificando con KNN
-# ################################################################################
-# print("------------------------------------------------------------------")
-# print("Clasificando con SVM")
-# print("------------------------------------------------------------------")
-
-# # Declaración de hiperparámetros
-# param_grid = {
-
-# }
-
-# # Test de hiperparámetros
-# clf = GridSearchCV(SVC(kernel="", class_weight=""), param_grid)
-# clf = clf.fit(x_train_pca, y_train)
-
-# print("Mejores hiperparámetros del modelo:")
-# print(clf.best_estimator_)
-
-# # ------------------------------------------------------------------------------
-# # Fit con CV
-# # kf = KFold(n_splits=10, shuffle=True)
-
-# for tr, tst in kf(x_train_pca):
-#     # Cogemos los datos de este kfold
-#     x_tr = x_train_pca[tr,:]
-#     y_tr = y_train[tr,:]
-#     x_tst = x_test_pca[tst,:]
-#     y_tst = y_test[tst,:]
-
-#     # Lanzamos el algoritmo
-
-#     # Mostramos la métrica sobre conjunto de validación
-
-# #############################
-# # Evaluación final del modelo
-# #############################
+sns.pairplot(df, hue="Pred").fig.suptitle("Componentes de PCA con etiquetas predichas", y=1.01)
+plt.show()
 
 
-# ################################################################################
-# # Clasificando con Random Forest
-# ################################################################################
-# print("------------------------------------------------------------------")
-# print("Clasificando con SVM")
-# print("------------------------------------------------------------------")
+################################################################################
+# Clasificando con KNN
+################################################################################
+print("------------------------------------------------------------------")
+print("Clasificando con KNN")
+print("------------------------------------------------------------------")
+
+# Declaración de hiperparámetros
+param_grid = {
+    "n_neighbors" : [3, 5, 7, 10, 13, 15],     # Diferentes valores de K
+    "weights": ["uniform", "distance"]   # Selección ponderada por distancia o no
+}
+
+# Test de hiperparámetros
+clf = GridSearchCV(KNeighborsClassifier(), param_grid)
+clf = clf.fit(x_train_pca, y_train)
+
+print("Mejores hiperparámetros del modelo:")
+print(clf.best_params_)
+print("\nMejor score obtenido:")
+print(clf.best_score_)
+
+# Guardamos el mejor estimador
+best_knn = clf.best_estimator_
+
+#############################
+# Evaluación final del modelo
+#############################
+
+y_pred = clf.predict(x_test_pca)
+
+print("\nResultados de la predicción sobre test:")
+print(classification_report(y_test, y_pred, target_names=labels_names))
+print("Matriz de confusión:")
+print(confusion_matrix(y_test, y_pred))
+
+# ------------------------------------------------------------------------------
+# Mostramos gráficos sobre los resultados
+disp = plot_confusion_matrix(best_knn, x_test_pca, y_test,
+                                display_labels=labels_names, cmap=plt.cm.Blues)
+plt.show()
 
 
-# # Declaración de hiperparámetros
-# param_grid = {
+df["Pred"] = y_pred
 
-# }
+sns.pairplot(df, hue="Labels").fig.suptitle("Componentes de PCA con sus verdaderas etiquetas", y=1.01)
+plt.show()
 
-# # Test de hiperparámetros
-# clf = GridSearchCV(SVC(kernel="", class_weight=""), param_grid)
-# clf = clf.fit(x_train_pca, y_train)
+sns.pairplot(df, hue="Pred").fig.suptitle("Componentes de PCA con etiquetas predichas", y=1.01)
+plt.show()
 
-# print("Mejores hiperparámetros del modelo:")
-# print(clf.best_estimator_)
+################################################################################
+# Clasificando con Random Forest
+################################################################################
+print("------------------------------------------------------------------")
+print("Clasificando con Random Forest")
+print("------------------------------------------------------------------")
 
-# # ------------------------------------------------------------------------------
-# # Fit con CV
-# # kf = KFold(n_splits=10, shuffle=True)
+# Declaración de hiperparámetros
+param_grid = {
+    "n_estimators" : [10, 50, 100, 200, 500],    # Nº de árboles
+    "max_depth" : [None, 20]    # Permitir crecer a los árboles o hacer poda
+}
 
-# for tr, tst in kf(x_train_pca):
-#     # Cogemos los datos de este kfold
-#     x_tr = x_train_pca[tr,:]
-#     y_tr = y_train[tr,:]
-#     x_tst = x_test_pca[tst,:]
-#     y_tst = y_test[tst,:]
+# Test de hiperparámetros
+clf = GridSearchCV(RandomForestClassifier(), param_grid)
+clf = clf.fit(x_train_pca, y_train)
 
-#     # Lanzamos el algoritmo
+print("Mejores hiperparámetros del modelo:")
+print(clf.best_params_)
+print("\nMejor score obtenido:")
+print(clf.best_score_)
 
-#     # Mostramos la métrica sobre conjunto de validación
+# Guardamos el mejor estimador
+best_rf = clf.best_estimator_
 
-# #############################
-# # Evaluación final del modelo
-# #############################
+#############################
+# Evaluación final del modelo
+#############################
+
+y_pred = clf.predict(x_test_pca)
+
+print("\nResultados de la predicción sobre test:")
+print(classification_report(y_test, y_pred, target_names=labels_names))
+print("Matriz de confusión:")
+print(confusion_matrix(y_test, y_pred))
+
+# ------------------------------------------------------------------------------
+# Mostramos gráficos sobre los resultados
+disp = plot_confusion_matrix(best_rf, x_test_pca, y_test,
+                                display_labels=labels_names, cmap=plt.cm.Blues)
+plt.show()
+
+df["Pred"] = y_pred
+
+sns.pairplot(df, hue="Labels").fig.suptitle("Componentes de PCA con sus verdaderas etiquetas", y=1.01)
+plt.show()
+
+sns.pairplot(df, hue="Pred").fig.suptitle("Componentes de PCA con etiquetas predichas", y=1.01)
+plt.show()
+
+
+# Los datos esta procesados con PCA y no son interpretables
+# Pero podemos ver la forma que tiene alguno de los árboles del
+# modelo
+# Función obtenida de: 
+# https://towardsdatascience.com/how-to-visualize-a-decision-tree-from-a-random-forest-in-python-using-scikit-learn-38ad2d75f21c
+
+from sklearn.tree import export_graphviz
+
+# Export as dot file
+export_graphviz(best_rf.estimators_[1], out_file='tree.dot', 
+                class_names = labels_names,
+                rounded = True, proportion = False, 
+                precision = 2, filled = True)
+
+# Convert to png using system command (requires Graphviz)
+from subprocess import call
+call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
+
+# Display in jupyter notebook
+from IPython.display import Image
+Image(filename = 'tree.png')
+
+# Como el mejor modelo no tiene límite de profundidad, vemos
+# que el árbol es bastante grande
